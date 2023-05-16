86b71a9dbfeb12aed7e3057198872ada77577350
Gasson205 <jerrygasson205@gmail.com>
2023-05-16 14:52:51 -0400

    Grade

---
+++ Gradesysytem.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your Grade"))
+if marks >= 90:
+    print("Grade is A+")
+elif marks >= 80:
+    print("Grade is B+")
+elif marks >= 70:
+    print("Grade is C+")
+elif marks >= 60:
+    print("Grade is D+")
+
